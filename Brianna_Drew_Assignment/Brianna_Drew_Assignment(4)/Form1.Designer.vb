ad5814df1ad6356d434d4c8d00d456d821390456
briannadrew <briannadrew@trentu.ca>
2019-02-05 23:05:26 -0500

    commit

---
+++ Brianna_Drew_Assignment/Brianna_Drew_Assignment(4)/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.UnsortedList = New System.Windows.Forms.ListBox()
+        Me.SortedList = New System.Windows.Forms.ListBox()
+        Me.ReadButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.AscendButton = New System.Windows.Forms.Button()
+        Me.DescendButton = New System.Windows.Forms.Button()
+        Me.UnsortedLabel = New System.Windows.Forms.Label()
+        Me.SortedLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'UnsortedList
+        '
+        Me.UnsortedList.BackColor = System.Drawing.Color.Black
+        Me.UnsortedList.Font = New System.Drawing.Font("Courier New", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.UnsortedList.ForeColor = System.Drawing.Color.White
+        Me.UnsortedList.FormattingEnabled = True
+        Me.UnsortedList.ItemHeight = 20
+        Me.UnsortedList.Location = New System.Drawing.Point(48, 60)
+        Me.UnsortedList.Name = "UnsortedList"
+        Me.UnsortedList.Size = New System.Drawing.Size(487, 624)
+        Me.UnsortedList.TabIndex = 0
+        '
+        'SortedList
+        '
+        Me.SortedList.BackColor = System.Drawing.Color.Black
+        Me.SortedList.Font = New System.Drawing.Font("Courier New", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SortedList.ForeColor = System.Drawing.Color.White
+        Me.SortedList.FormattingEnabled = True
+        Me.SortedList.ItemHeight = 20
+        Me.SortedList.Location = New System.Drawing.Point(869, 60)
+        Me.SortedList.Name = "SortedList"
+        Me.SortedList.Size = New System.Drawing.Size(487, 624)
+        Me.SortedList.TabIndex = 1
+        '
+        'ReadButton
+        '
+        Me.ReadButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ReadButton.ForeColor = System.Drawing.Color.Blue
+        Me.ReadButton.Location = New System.Drawing.Point(156, 706)
+        Me.ReadButton.Name = "ReadButton"
+        Me.ReadButton.Size = New System.Drawing.Size(236, 37)
+        Me.ReadButton.TabIndex = 2
+        Me.ReadButton.Text = "Read in names.txt"
+        Me.ReadButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExitButton.ForeColor = System.Drawing.Color.Blue
+        Me.ExitButton.Location = New System.Drawing.Point(1006, 706)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(223, 37)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'AscendButton
+        '
+        Me.AscendButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AscendButton.ForeColor = System.Drawing.Color.Blue
+        Me.AscendButton.Location = New System.Drawing.Point(590, 221)
+        Me.AscendButton.Name = "AscendButton"
+        Me.AscendButton.Size = New System.Drawing.Size(223, 103)
+        Me.AscendButton.TabIndex = 4
+        Me.AscendButton.Text = "Ascending Order"
+        Me.AscendButton.UseVisualStyleBackColor = True
+        '
+        'DescendButton
+        '
+        Me.DescendButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DescendButton.ForeColor = System.Drawing.Color.Blue
+        Me.DescendButton.Location = New System.Drawing.Point(590, 433)
+        Me.DescendButton.Name = "DescendButton"
+        Me.DescendButton.Size = New System.Drawing.Size(223, 103)
+        Me.DescendButton.TabIndex = 5
+        Me.DescendButton.Text = "Descending Order"
+        Me.DescendButton.UseVisualStyleBackColor = True
+        '
+        'UnsortedLabel
+        '
+        Me.UnsortedLabel.AutoSize = True
+        Me.UnsortedLabel.BackColor = System.Drawing.Color.Transparent
+        Me.UnsortedLabel.Font = New System.Drawing.Font("Courier New", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.UnsortedLabel.ForeColor = System.Drawing.Color.Red
+        Me.UnsortedLabel.Location = New System.Drawing.Point(43, 18)
+        Me.UnsortedLabel.Name = "UnsortedLabel"
+        Me.UnsortedLabel.Size = New System.Drawing.Size(418, 29)
+        Me.UnsortedLabel.TabIndex = 6
+        Me.UnsortedLabel.Text = "Original Text File Content:"
+        '
+        'SortedLabel
+        '
+        Me.SortedLabel.AutoSize = True
+        Me.SortedLabel.BackColor = System.Drawing.Color.Transparent
+        Me.SortedLabel.Font = New System.Drawing.Font("Courier New", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SortedLabel.ForeColor = System.Drawing.Color.Red
+        Me.SortedLabel.Location = New System.Drawing.Point(864, 18)
+        Me.SortedLabel.Name = "SortedLabel"
+        Me.SortedLabel.Size = New System.Drawing.Size(388, 29)
+        Me.SortedLabel.TabIndex = 7
+        Me.SortedLabel.Text = "Sorted Text File Content:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Brianna_Drew_Assignment_4_.My.Resources.Resources._1
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1405, 755)
+        Me.Controls.Add(Me.SortedLabel)
+        Me.Controls.Add(Me.UnsortedLabel)
+        Me.Controls.Add(Me.DescendButton)
+        Me.Controls.Add(Me.AscendButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ReadButton)
+        Me.Controls.Add(Me.SortedList)
+        Me.Controls.Add(Me.UnsortedList)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Text File Read & Sort"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents UnsortedList As ListBox
+    Friend WithEvents SortedList As ListBox
+    Friend WithEvents ReadButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents AscendButton As Button
+    Friend WithEvents DescendButton As Button
+    Friend WithEvents UnsortedLabel As Label
+    Friend WithEvents SortedLabel As Label
+End Class
---
+++ Brianna_Drew_Assignment/Brianna_Drew_Assignment(4)/Form1.vb
@@ -0,0 +1,148 @@
+﻿'
+' This is a form that reads and sorts a .txt file.
+'
+' Written by: Brianna Drew (0622446) - March 2018
+'
+' Purpose: To read a text file from computer, read each line into program, bubble sort each line into either ascending or descending order (alphabetically).
+'
+' Parameters: None.
+'
+' Subroutine Required: The form itself loading has a subroutine as well as each button on the form has it's own subroutine. There is also a subroutine
+'                      called from either the "Ascending Order" or "Descending Order" button click subroutines to add the items to the desgnated list
+'                      box after they have been bubble sorted.
+'
+' Modifications: - March 11/18 - Program started, entire program written other than the comments.
+'                - March 27/18 - Comments added.
+'                - March 31/18 - Re-did bubble sorts to be "true" bubble sort, comments updated.
+'                - April 4/18 - Final comments added.
+
+Public Class Form1
+
+
+    ' This section of code defines the variables required (at class-level).
+    Dim NameLine(0 To 100) As String ' Definition for variable (string array) that will hold each line read from the text file (up to 100 items).
+    Dim counter As Integer = 99 ' Definition for variable that will count how many lines are being read from the text file.
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' This subroutine runs when the form (program) first loads.
+
+        AscendButton.Enabled = False ' The button to sort the items read in from the text file in ascending order is disabled to start because the file
+        ' has not been read in yet.
+        DescendButton.Enabled = False ' The button to sort the items read in from the text file in descending order is disabled to start because the file
+        ' has not been read in yet.
+
+    End Sub
+
+
+    Private Sub ReadButton_Click(sender As Object, e As EventArgs) Handles ReadButton.Click
+        ' This subroutine runs when the "Read in names.txt" button is clicked.
+
+        UnsortedList.Items.Clear() ' Clear the list box that holds the unsorted items read from the text file.
+
+        counter = 0 ' Initialize the counter variable to 0 for this specific subroutine.
+        Dim Filename As String ' Definition for variable that will hold the location of the text file.
+        Dim End_of_File = -1 ' Definition for variable that holds the how many items in the text file remain when nothing actually remains (when it
+        ' reaches the end of the text file).
+        Dim Good_Filename As Boolean ' Definition for variable that will hold whether or not the file name exists.
+        Dim InputFile As System.IO.StreamReader ' Defintion for variable that will hold the text file to be read.
+
+        Filename = "C:\\Brianna_Drew_Assignment(4)\names.txt" ' Specifying the the location of the text file and assigning the path to the appropriate
+        ' variable.
+        Good_Filename = System.IO.File.Exists(Filename) ' Checking to see if the file name (path) exists, and will return a boolean as the result and
+        ' assign it to the appropriate variable.
+
+        If (Not Good_Filename) Then ' If the file given does not exist, then...
+            UnsortedList.Items.Add("The selected file does not exist:") ' Add an error message to the Unsorted list box.
+            UnsortedList.Items.Add("Name: " & Filename) ' Show the name (path) of the file given in the Unsorted list box.
+        End If
+
+        InputFile = My.Computer.FileSystem.OpenTextFileReader("C:\\Brianna_Drew_Assignment(4)\names.txt") ' Open the specified file and name it under 
+        ' the variable "InputFile".
+
+        Do While InputFile.Peek <> End_of_File ' Run the following code until there is no lines of text remaining in the text file.
+            NameLine(counter) = InputFile.ReadLine ' Assign the current line from the text file to the counter index in the NameLine array.
+            UnsortedList.Items.Add(NameLine(counter)) ' Add the item from the array to the Unsorted list box.
+            counter = counter + 1 ' Add 1 to the counter (so the next line in the file can be assigned to the next place in the NameLine array).
+        Loop
+
+        AscendButton.Enabled = True ' Enable the "Ascending Order" button now that there are items read from text file to be able to be sorted.
+        DescendButton.Enabled = True ' Enable the "Descending Order" button now that there are items read from text file to be able to be sorted.
+
+        InputFile.Close() ' Close the text file.
+
+    End Sub
+
+
+    Private Sub AscendButton_Click(sender As Object, e As EventArgs) Handles AscendButton.Click
+        ' This subroutine runs when the "Ascending Order" button is clicked.
+
+        Dim k, j As Integer
+        Dim tempname As String
+
+        ''''BUBBLE SORT''''
+        For k = 2 To counter ' Go through the all the items in the NameLine array the appropriate number of times...
+            For j = 0 To (counter - k) ' Go through all the items in the NameLine array and...
+                If NameLine(j) > NameLine(j + 1) Then ' If the current item in the array is alphabetically greater than the following item, then...
+                    tempname = NameLine(j + 1) ' Assign the item following the current item in the array to a "temporary" variable.
+                    NameLine(j + 1) = NameLine(j) ' Assign the current item in the array to the original place of the item that was following the current
+                    ' item in the array.
+                    NameLine(j) = tempname ' Assign the item that was following the current item (now in the temporary variable) to the place in the array
+                    ' in which the current item was.
+                    ' Basically, if the name is "greater" than the name after it (alphabetically), they switch places in the array.
+                End If
+            Next j
+        Next k
+
+        DisplayNames() ' Call the DisplayNames subroutine to add the sorted items to the Sorted list box.
+
+    End Sub
+
+
+    Private Sub DescendButton_Click(sender As Object, e As EventArgs) Handles DescendButton.Click
+        ' This subroutine runs when the "Descending Order" button is clicked.
+
+        Dim k, j As Integer ' Definition for variables that will serve as other counters (to be starting points in the following for loops).
+        Dim tempname As String
+
+        ''''BUBBLE SORT''''
+        For k = 1 To counter ' Go through the all the items in the NameLine array the appropriate number of times...
+            For j = 0 To (counter - k) ' Go through all the items in the NameLine array and...
+                If NameLine(j) < NameLine(j + 1) Then ' If the current item in the array is alphabetically less than the following item, then...
+                    tempname = NameLine(j) ' Assign the current item in the array to a "temporary" variable.
+                    NameLine(j) = NameLine(j + 1) ' Assign the item that was following the current item in the array to the original place of the current
+                    ' item in the array.
+                    NameLine(j + 1) = tempname ' Assign the current item (now in the temporary variable) to the place in the array in which the item
+                    ' following the current item was.
+                    ' Basically, if the name is "less" than the name after it (alphabetically), they switch places in the array. 
+                End If
+            Next j
+        Next k
+
+        DisplayNames() ' Call the DisplayNames subroutine to add the sorted items to the Sorted list box.
+
+    End Sub
+
+    Sub DisplayNames()
+        ' This subroutine runs when it is called by either the AscendButton_Click method or the DescendButton_Click method to add the items to the 
+        ' sorted list box after they have been sorted alphabetically via bubble sort.
+
+        SortedList.Items.Clear() ' Clear the list box that holds the items read from the text file after they have been sorted.
+
+        Dim counter1 As Integer ' Definition for variable that will serve as another counter (to be a starting point in the following for loop).
+
+        For counter1 = 0 To (counter - 1) ' Go through all the items in the NameLine array and...
+            SortedList.Items.Add(NameLine(counter1)) ' add them each to the Sorted 
+        Next
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        ' This subroutine runs when the "Exit" button is clicked.
+
+        Me.Close() ' Closes the program.
+
+    End Sub
+
+
+End Class
